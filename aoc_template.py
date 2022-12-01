549cc6f29b2ad4399a8b209de5e429bdbaf55eec
ryanbarker <ryan.barker@kogan.com.au>
2022-12-01 16:57:57 +1000

    init template

---
+++ aoc_template.py
@@ -0,0 +1,22 @@
+from aocd import get_data
+
+
+def p1(data):
+    return
+
+
+def p2(data):
+    return
+
+
+def main():
+    data = get_data(day=1, year=2022).splitlines()
+
+    print(data, type(data))
+    print()
+    print(f"p1: {p1(data)}")
+    print(f"p2: {p2(data)}")
+
+
+if __name__ == "__main__":
+    main()
